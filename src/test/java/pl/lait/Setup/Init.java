d6cbb31461d379efe02a9b75015896ead4ccb69f
Adam <lasicmk2@gmail.com>
2017-06-24 13:21:18 +0200

    zmiana adresu IP na docker

--- src/test/java/pl/lait/Setup/Init.java
+++ src/test/java/pl/lait/Setup/Init.java
@@ -5,6 +5,7 @@ import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.After;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -24,25 +25,26 @@ public class Init {
 			
 			try {
 				//po uruchomieniu huba i noda (java -jar seleniuserver...) ustawiamy jego adres IP (localhost - nasz komputer, wszystko na tej samej maszynie)
-				hubUrl = new URL("http://localhost:4444/wd/hub/"); 
+				hubUrl = new URL("http://192.168.0.112:4444/wd/hub/"); 
 			} catch (MalformedURLException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 			
 			// zmieniamy firefoxDriver na WebDriver więc trzeba też podać jakiej przeglądarki oczekujemy
 			DesiredCapabilities cap = DesiredCapabilities.firefox();
-			
+						
 			//wyłączamy stary sposób i wskazujemy nowy - RemoteWebDriver - podająć w parametrach gdzie ma wołać po przeglądarkę i jaką
 			//driver = new FirefoxDriver();
 			driver = new RemoteWebDriver(hubUrl, cap);
 			
-			driver.get("http://newtours.demoaut.com");
 			
+			driver.get("http://newtours.demoaut.com");
+
 			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 			return driver;
 		}else{
 			return driver;
+			
 		}
 	}
 	
